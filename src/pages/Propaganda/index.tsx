ab888c6e6e2519b1189acf91466933512406a4b8
silvio <silvioquintana1@hotmail.com>
2025-06-13 23:03:55 -0300

    criar conta no site

--- src/pages/Propaganda/index.tsx
+++ src/pages/Propaganda/index.tsx
@@ -179,7 +179,7 @@ export default function Anuncios() {
       <FeaturesScene />
       <TestimonialScene />
       <CtaScene />
-      <AdScene />
+      {/* <AdScene /> */}
       {/* <TilinhoLanchesScene /> */}
     </div>
   );
